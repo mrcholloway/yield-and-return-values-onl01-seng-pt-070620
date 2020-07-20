3dae12c718fe0c8b2a0b5bae5fc4d182fb30dcd2
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-20 01:02:48 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,19 @@
-require 'pry'
-
-def hello(array)
+def my_each(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i += 1
+    i = i + 1
   end
 end
 
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(list) {|i| "Hello #{i} what's up?"}
